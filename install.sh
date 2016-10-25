c94908357357520e515175147db24cc3bc1c4571
Pander <pander@users.sourceforge.net>
2016-10-25 16:17:54 +0200

    Fixed installation script bug

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 cp -f en_NL /usr/share/i18n/locales/
 
 # Register English locale for the Netherlands
-if [ `grep '^en_NL /etc/locale.gen'|wc -l` -eq 0 ]
+if [ `grep ^en_NL /etc/locale.gen|wc -l` -eq 0 ]
 then
     echo "en_NL.UTF-8 UTF-8" >> /etc/locale.gen
 fi
