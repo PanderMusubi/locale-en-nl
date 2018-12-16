168f7508190a7de51cc97cdb8701a02c9be0c91f
PanderMusubi <pander@users.sourceforge.net>
2018-12-16 15:59:50 +0100

    improved analysis

--- analysis/2-execution-flags.sh
+++ analysis/2-execution-flags.sh
@@ -1,3 +1,3 @@
-for i in `find glibc -type f -executable -print|grep -v .git/|grep -v .sh$|grep -v .py$|grep -v .pl$|grep -v scripts/|grep -v configure$`; do
+for i in `find glibc -type f -executable -print|grep -v '\.git/'|grep -v '\.sh$'|grep -v '\.ksh$'|grep -v '\.py$'|grep -v '\.pl$'|grep -v 'scripts/'|grep -v 'configure$'`; do
 	ls -l $i
 done
