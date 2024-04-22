4313a2392829cd58e7b9989bdc28d035a80fac5f
Pander <pander@users.sourceforge.net>
2024-04-22 17:26:39 +0200

    fix version and error reporting

--- add.sh
+++ add.sh
@@ -8,10 +8,10 @@ if [ `id -u` -ne 0 ]; then
 fi
 
 # add locale
-if [ -d /usr/share/i18n/ ]; then
+if [ -d /usr/share/i18n/locales/ ]; then
     cp -f en_NL /usr/share/i18n/locales/
 else
-    echo "ERROR: No /usr/share/i18n/ directory, install glibc-i18ndata"
+    echo "ERROR: No /usr/share/i18n/locales/ directory, install package locales or glibc-i18ndata"
     exit 1
 fi
 
@@ -40,6 +40,7 @@ fi
 
 # regenerate locales
 locale-gen
+#TODO where to use the following lines?
 #elif [ -x /usr/bin/localedef ]; then
 #    /usr/bin/localedef -f UTF-8 -i en_NL en_NL.UTF-8
 #else
